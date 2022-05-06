522fcbe8f57bacad1d3eb85db658b8a736579661
Akira Kubo <akira.kubo@konicaminolta.com>
2022-05-06 22:00:45 +0900

    ﻿:+1:to Add Program.cs
    Commit body...

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using GettingStarted;
+using Kamishibai;
+using Microsoft.Extensions.Hosting;
+
+var builder = KamishibaiApplication<App,MainWindow>.CreateBuilder();
+
+var app = builder.Build();
+app.RunAsync();
