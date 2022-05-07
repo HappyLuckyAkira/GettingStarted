3a76fd912568f09e365d6cc3d6ecef0a3678a94d
Akira Kubo <akira.kubo@konicaminolta.com>
2022-05-07 23:08:19 +0900

    ﻿:+1:paring view & ViewModel
    Commit body...

--- Program.cs
+++ Program.cs
@@ -4,5 +4,10 @@ using Microsoft.Extensions.Hosting;
 
 var builder = KamishibaiApplication<App,MainWindow>.CreateBuilder();
 
+builder.Services.AddPresentation<MainWindow, MainViewModel>();
+builder.Services.AddPresentation<FirstPage, FirstViewModel>();
+
+
 var app = builder.Build();
 app.RunAsync();
+
