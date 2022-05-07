0e5e85a8da49d12127c8da26fb928fa3f09f12b0
Akira Kubo <akira.kubo@konicaminolta.com>
2022-05-07 22:58:37 +0900

    ﻿:+1:add Navigate
    Commit body...

--- MainViewModel.cs
+++ MainViewModel.cs
@@ -4,9 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Windows.Input;
+using Microsoft.Toolkit.Mvvm.Input;
 namespace GettingStarted
 {
-    internal class MainViewModel
+    public class MainViewModel
     {
+        private readonly IPresentationService _presentationService;
+
+        public MainViewModel(IPresentationService presentationService)
+        {
+            _presentationService = presentationService;
+        }
+        public ICommand NavigateCommand => new AsyncRelayCommand(Navigate);
+
+        private Task Navigate() => _presentationService.NavigateToFirstAsync();
     }
 }
