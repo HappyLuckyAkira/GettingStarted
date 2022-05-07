50b7e808aa6aa484fcada4e323f6542db3ac9e9f
Akira Kubo <akira.kubo@konicaminolta.com>
2022-05-07 23:11:18 +0900

    ﻿:+1:add message to FirstViewModel
    Commit body...

--- FirstViewModel.cs
+++ FirstViewModel.cs
@@ -10,5 +10,10 @@ namespace GettingStarted
     [Navigate]
     public class FirstViewModel
     {
+        public FirstViewModel(string message)
+        {
+            Message = message;
+        }
+        public string Message { get; }
     }
 }
